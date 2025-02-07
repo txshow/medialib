c7fa8d2c212393c8ad1c2cb74ac57fc6f0b4a8a8
txshow <60607177+txshow@users.noreply.github.com>
2025-02-07 13:40:09 +0800

    Delete mediainfo directory

--- mediainfo/src/main/java/io/github/anilbeesetti/nextlib/mediainfo/Chapter.kt
+++
@@ -1,8 +0,0 @@
-package io.github.anilbeesetti.nextlib.mediainfo
-
-data class Chapter(
-  val index: Int,
-  val start: Long,
-  val end: Long,
-  val title: String?
-)
--- mediainfo/src/main/java/io/github/anilbeesetti/nextlib/mediainfo/SubtitleStream.kt
+++
@@ -1,9 +0,0 @@
-package io.github.anilbeesetti.nextlib.mediainfo
-
-data class SubtitleStream(
-    val index: Int,
-    val title: String?,
-    val codecName: String,
-    val language: String?,
-    val disposition: Int
-)
